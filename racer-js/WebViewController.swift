a8614c3ee740c69040513341889b62ddaed9a0c3
Miguel Cazares <miguel.cazares@shipstation.com>
2017-07-31 19:41:13 -0500

    get basic view to show up

---
+++ racer-js/WebViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewController.swift
+//  racer-js
+//
+//  Created by Miguel Cazares on 7/31/17.
+//  Copyright © 2017 Cazares. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
