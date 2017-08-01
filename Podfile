487f77bb066c62447f89a5cd1751ba108621e584
Miguel Cazares <miguel.cazares@shipstation.com>
2017-07-31 19:58:55 -0500

    more project setup

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '10.0'
+
+inhibit_all_warnings!
+
+def shared_pods
+	pod 'LHSCategoryCollection', '~> 0.0'
+	pod 'SVProgressHUD', '~> 1.1'
+	use_frameworks!
+end
+
+target 'racer-js' do
+	shared_pods
+end
--- racer-js/WebViewController.swift
+++ racer-js/WebViewController.swift
@@ -12,7 +12,8 @@ class WebViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .black
+        
     }
 }
 
